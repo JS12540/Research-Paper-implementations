add83e54f7f503c9ade947177c7c7c0ede5b7873
jayshah0726 <jay.shah@elixirequities.com>
2025-03-09 18:53:17 +0530

    Added Bert langauge model

--- BERT/BERT_pytorch/model/utils/__init__.py
+++ BERT/BERT_pytorch/model/utils/__init__.py
@@ -1,4 +1,4 @@
-from .feed_forward import PositionwiseFeedForward
+from .feed_forward import PositionWiseFeedForward
 from .layer_norm import LayerNorm
 from .sublayer import SublayerConnection
 from .gelu import GELU
